c70da0c8fe5461d895e03e24d923319dcd0bc24b
EugeneBUSUEK <Eugeny.BUSUEK@gmail.com>
2023-04-08 14:51:30 +0300

    build: inti project

---
+++ src/main/java/com/tinkoffworkshop/jirello/JirelloApplication.java
@@ -0,0 +1,13 @@
+package com.tinkoffworkshop.jirello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JirelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JirelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinkoffworkshop/jirello/JirelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinkoffworkshop.jirello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JirelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
