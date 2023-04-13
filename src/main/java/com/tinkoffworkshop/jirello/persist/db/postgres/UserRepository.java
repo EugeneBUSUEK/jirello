1b5fb483aa514b826c5360300d3897b74de4bebd
EugeneBUSUEK <Eugeny.BUSUEK@gmail.com>
2023-04-13 23:09:26 +0300

    feat: add Jwt dependencies, create JwtService, init JwtAuthenticationFilter

--- src/main/java/com/tinkoffworkshop/jirello/persist/db/postgres/UserRepository.java
+++ src/main/java/com/tinkoffworkshop/jirello/persist/db/postgres/UserRepository.java
@@ -1,18 +1,21 @@
 package com.tinkoffworkshop.jirello.persist.db.postgres;
 
 import com.tinkoffworkshop.jirello.persist.db.postgres.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     @Override
     <S extends UserEntity> S save(S entity);
 
     @Override
     Optional<UserEntity> findById(Long aLong);
+
+    Optional<UserEntity> findByEmail(String email);
 
     @Override
     List<UserEntity> findAll();
